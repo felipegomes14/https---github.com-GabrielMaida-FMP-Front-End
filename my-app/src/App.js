719e4f81671dc5be2fff409165d282e83f3b4187
Your Name <you@example.com>
2024-10-17 17:37:02 -0300

    bruto

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
+import Home from './pages/home.js';
+import About from './pages/about.js';
 import Contact from './pages/contact.js';
 
 function App() {
